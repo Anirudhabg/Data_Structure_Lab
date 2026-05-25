29416f0a51fc34ca6cdcbe28813ebbc8d9a8211a
Anirudhabgs <anirudhabg@gmail.com>
2026-05-25 14:17:33 +0530

    Made changes in Queue

--- Program3/playground.c
+++ Program3/playground.c
@@ -6,54 +6,38 @@
 
 #define MAX 5
 
-void insert();
-void delete();
-void display();
-
 int QUEUE[MAX];
 int REAR = -1;
 int FRONT = -1;
 
-void insert()
-{
-    if (REAR == MAX - 1)
-    {
+void enqueue(int val){
+    if(REAR == MAX - 1){
         printf("Queue is Full\n");
         return;
     }
-    if (FRONT == -1)
-    {
+    if(FRONT == -1){
         FRONT = 0;
     }
-    printf("\n Insert: ");
-    int item;
-    scanf("%d", &item);
-    QUEUE[++REAR] = item;
+    QUEUE[++REAR] = val;
     printf("\n Item Inserted to Queue\n");
 }
 
-void delete()
-{
-    if (FRONT == -1 || FRONT > REAR)
-    {
+void dequeue(){
+    if(FRONT == -1 || FRONT > REAR){
         printf("Queue is Empty\n");
         return;
     }
     int item = QUEUE[FRONT++];
-
     printf("\n Delete: %d", item);
 }
 
-void display()
-{
-    if (FRONT == -1 || FRONT > REAR)
-    {
+void display(){
+    if(FRONT == -1 || FRONT > REAR){
         printf("Queue is Empty\n");
         return;
     }
     printf("\n Queue: \t");
-    for (int i = FRONT; i <= REAR; i++)
-    {
+    for(int i = FRONT; i <= REAR; i++){
         printf("%d\t", QUEUE[i]);
     }
     printf("\n");
@@ -61,27 +45,14 @@ void display()
 
 void main()
 {
-    int choice;
-    while (1)
-    {
-        printf("\n1. Insert\t 2. Delete\t 3. Display\t 4. Exit\n Choose any one option: ");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            insert();
-            break;
-        case 2:
-            delete ();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            exit(1);
-            break;
-        default:
-            printf("Invalid Choice!\n");
-        }
-    }
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    dequeue();
+    display();
+    enqueue(40);
+    enqueue(50);
+    enqueue(60);
+    display();
 }
